d2774a0ca02f35694bb37ddf26bd776d62f7f028
Colin Alexander <chalexander@me.com>
2019-08-22 16:18:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,11 @@
 def map(array)
   count = 0
+  result = []
   while array[count] do
-    
+    result.push(yield(array[count]))
     count += 1
   end
-  
+  return result
 end
 
 def reduce(array, starting_value = 0) |block|
