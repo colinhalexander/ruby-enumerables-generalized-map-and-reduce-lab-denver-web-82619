49a4776ed66396b2a48a6f5c803eeab94e42bc43
Colin Alexander <chalexander@me.com>
2019-08-22 16:30:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,12 +11,11 @@ end
 def reduce(array, starting_value = 0)
   count = 0
   result = starting_value
-  while array[count + 1] do
-    if starting_value == 0
-      result = yield(array[count], array[count + 1])
+  while array[count] do
+      result = yield(result, array[count])
       count += 1
     else 
-    
+      
     end
   end
   return result
